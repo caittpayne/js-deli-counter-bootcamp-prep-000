7b214c07584e07986b93d9802447b55df2ef3fa1
Cait Payne <caitpayne@outlook.com>
2018-04-04 23:47:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   
-  return ('Welcome, '+ name + '. You are number '+ katzDeli.length +' in line.')
+  return ('Welcome, '+ name + '. You are number '+ katzDeliLine.length +' in line.')
 }
