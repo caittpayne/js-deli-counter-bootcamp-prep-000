5695f167c7a099e334c61761ead61a5fb3e7b154
Cait Payne <caitpayne@outlook.com>
2018-04-05 00:04:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine) {
   if(katzDeliLine.length > 0) {
-    katzDeliLine.unshift();
+    katzDeliLine.shift();
     
     return (`Currently serving ${katzDeliLine[0]}.`)
   }
