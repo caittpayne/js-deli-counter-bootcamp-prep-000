9df3304879c2eba678db955c53ca4628a422b3a6
Cait Payne <caitpayne@outlook.com>
2018-04-05 01:23:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
-  if(katzDeliLine.length > 0) {
-    return(`Currently serving ${katzDeliLine[0]}.`)
-  }
+  return(`Currently serving ${katzDeliLine[0]}.`)
   
-  else{
+  function nextInLine() {
+   if(katzDeliLine.length > 0) {
+     katzDeliLine.shift();
+  }
+  else {
     return('There is nobody waiting to be served!')
+  }
   }
 }
