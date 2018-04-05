3d42bd84c9302ef16a0c9e824b13b8a17f178bd1
Cait Payne <caitpayne@outlook.com>
2018-04-05 01:28:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ function nowServing(katzDeliLine) {
   while(katzDeliLine.length <= 0) {
     return('There is nobody waiting to be served!')
   }
+  while(katzDeliLine.length > 0) {
+    return(`Currently serving ${katzDeliLine[0]}`)
+  }
+  katzDeliLine.shift();
 }
