aab7c480ddcde59bea31f8844bbac67927db1890
Cait Payne <caitpayne@outlook.com>
2018-04-05 01:48:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  if(katzDeliLine.length > 0) {
-  return('The line is currently: '+ katzDeliLine.slice(0) +'')
+  if(katzDeliLine.length === 0) {
+  return('The line is currently empty.')
 }
 else {
-  return('The line is currently empty.')
+  const lineLength =
 }
 }
